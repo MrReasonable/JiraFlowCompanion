e1b8b82cb5434bd43c83c58920506f7698ae1f82
Magnus Siverbrant <siven@paddelkraft.se>
2017-05-15 21:28:30 +0200

    Add tests for parsing Jira URL:s

---
+++ src/test/spec/jiraUtilSpec.js
@@ -0,0 +1,10 @@
+describe("Parse angular query sting ", function() {
+	
+	approveIt("should parse standard query string ", function(approvals) {
+		approvals.verify(JiraUrl().parseAngularQueryString("rapidView=570&swimlane=1226,1225&column=2988,2970,2964,2965,2966"));
+	});
+
+	approveIt("should handle parsing errors gracefully", function(approvals) {
+		approvals.verify(JiraUrl().parseAngularQueryString("rapidView=570&swimlane=1226,1225&column=2988,2970,2964,2965,2966&from=2017-04-01"));
+	});
+});
